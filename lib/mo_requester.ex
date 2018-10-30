c8137d16708f43495d2c47eab2ca56118104a520
Maarten Jacobs <m.jacobs@defacto.nl>
2018-10-30 16:04:42 +0100

    implement request tester

--- lib/mo_requester.ex
+++ lib/mo_requester.ex
@@ -2,17 +2,78 @@ defmodule MoRequester do
   @moduledoc """
   Documentation for MoRequester.
   """
+  import Meeseeks.XPath
+
+  @mo_url System.get_env("MO_URL") || "<INSERT MO_URL here>"
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MoRequester.hello()
-      :world
 
   """
-  def hello do
-    :world
+  def request() do
+    HTTPoison.start()
+
+    document =
+      @mo_url
+      |> HTTPoison.get!()
+      |> Map.get(:body)
+      |> Meeseeks.parse(:xml)
+
+    result_code =
+      document
+      |> Meeseeks.one(xpath("/ProcessResults_Result/ResultCode/text()"))
+      |> Meeseeks.tree()
+
+    error_number =
+      document
+      |> Meeseeks.one(xpath("/ProcessResults_Result/ErrorNumber/text()"))
+      |> Meeseeks.tree()
+
+    {result_code, error_number}
+  end
+
+  def request(true) do
+    HTTPoison.start()
+
+    {time, %{body: response_body}} =
+      :timer.tc(fn ->
+        HTTPoison.get!(@mo_url)
+      end)
+
+    document =
+      response_body
+      |> Meeseeks.parse(:xml)
+
+    result_code =
+      document
+      |> Meeseeks.one(xpath("/ProcessResults_Result/ResultCode/text()"))
+      |> Meeseeks.tree()
+
+    error_number =
+      document
+      |> Meeseeks.one(xpath("/ProcessResults_Result/ErrorNumber/text()"))
+      |> Meeseeks.tree()
+
+    {result_code, error_number, time}
+  end
+
+  def sync_requests(number_of_requests) do
+    :timer.tc(fn ->
+      1..number_of_requests
+      |> Enum.to_list()
+      |> Enum.map(fn _ ->
+        request()
+      end)
+    end)
+  end
+
+  def async_requests(number_of_requests) do
+    :timer.tc(fn ->
+      1..number_of_requests
+      |> Enum.to_list()
+      |> Task.async_stream(fn _ ->
+        request()
+      end)
+      |> Enum.map(fn {:ok, result} -> result end)
+    end)
   end
 end
--- test/mo_requester_test.exs
+++ test/mo_requester_test.exs
@@ -2,7 +2,25 @@ defmodule MoRequesterTest do
   use ExUnit.Case
   doctest MoRequester
 
-  test "greets the world" do
-    assert MoRequester.hello() == :world
+  test "does a request" do
+    assert {"Ok", "0"} == MoRequester.request()
+  end
+
+  test "does 5 sync requests within 2 secs" do
+    assert {time, values} = MoRequester.sync_requests(5)
+    assert time < 2_000_000
+    assert values == 1..5 |> Enum.to_list() |> Enum.map(fn _ -> {"Ok", "0"} end)
+  end
+
+  test "does 5 async requests within 2 secs" do
+    assert {time, values} = MoRequester.async_requests(5)
+    assert time < 2_000_000
+    assert values == 1..5 |> Enum.to_list() |> Enum.map(fn _ -> {"Ok", "0"} end)
+  end
+
+  test "does 100 async requests within 5 secs" do
+    assert {time, values} = MoRequester.async_requests(100)
+    assert time < 5_000_000
+    assert values == 1..100 |> Enum.to_list() |> Enum.map(fn _ -> {"Ok", "0"} end)
   end
 end
